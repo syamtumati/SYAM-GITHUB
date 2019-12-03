48a81ade8809961e46c38c194cd9fb80c6cdb959
syamtumati <syam.rlx@gmail.com>
2019-12-03 22:53:54 +0000

    Testing

--- kubernetes/terraform/awsinstance.tf
+++ kubernetes/terraform/awsinstance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-access_key = "AKIAT3GNA7XJCCYQE5UP"
-secret_key = "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"
+access_key = "AKIAT3GNA7XJCCYADEC"
+secret_key = "gmvdfy4Xt0iYQ2w3DmCKA1PuRJbhv2bGO2vwWDE"
 region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-ami = "ami-00dc79254d0461090"
+ami = "ami-00dc79254d04610"
 instance_type = "t2.micro"
 tags {
 Name = "syam_testing"
